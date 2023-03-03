a46ef4ead1ebc29c13b8f929a37ce1b87f530b2b
liahoney <toridoc@gmail.com>
2023-03-03 09:26:22 +0900

    modify

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import MyDropDownPicker from "./src/MyDropDownPicker";
+import TextInputModal from "./src/TextInputModal";
+
+import { useGallery } from "./src/use-gallery";
+
+const width = Dimensions.get("screen").width;
+const columnSize = width / 3;
 
 export default function App() {
+  const { 
+    // images, 
+    pickImage, 
+    deleteImage, 
+    imagesWithAddButton, 
+    selectedAlbum,
+    modalVisible,
+        openModal,
+        closeModal, 
+        albumTitle,
+        setAlbumTitle,
+        addAlbum,
+        resetAlbumTitle 
+  } = useGallery();
+
+  const onPressOpenGallery = () => {
+    pickImage();
+  };
+
+  const onLongPressImage = (imageId) => deleteImage(imageId);
+
+  const renderItem = ({ item: { id, uri }, index }) => {
+    if (id === -1) {
+      return (
+        <TouchableOpacity
+          onPress={onPressOpenGallery}
+          style={{
+            width: columnSize,
+            height: columnSize,
+            backgroundColor: "lightgrey",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontWeight: "100", fontSize: 45 }}>+</Text>
+        </TouchableOpacity>
+      );
+    }
+
+    return (
+      <TouchableOpacity onLongPress={() => onLongPressImage(id)}>
+        <Image
+          source={{ uri }}
+          style={{ width: columnSize, height: columnSize }}
+        />
+      </TouchableOpacity>
+    );
+  };
+
+  const onPressAddAlbum = () => {
+    openModal()
+  }
+
+  const onSubmitEditing = () => {
+    //1.앨범에 타이틀 추가
+    addAlbum()
+    //2.모달 닫기 &TextInput의 value초기화
+    closeModal();
+    resetAlbumTitle(); 
+    
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    
+      {/* <Button title="갤러리 열기" onPress={onPressOpenGallery} /> */}
+
+      {/* 앨범 DropDown, 앨범 추가 버튼  */}
+      <MyDropDownPicker selectedAlbumTitle={selectedAlbum.title} onPressAddAlbum={onPressAddAlbum}/>
+
+      {/* 앨범을 추가하는 TextInputModal */}
+      <TextInputModal 
+        modalVisible={modalVisible}
+        albumTitle={albumTitle}
+        setAlbumTitle={setAlbumTitle}
+        onSubmitEditing={onSubmitEditing}
+        onPressBackdrop={onPressBackdrop}
+      />
+      <FlatList
+        data={imagesWithAddButton}
+        renderItem={renderItem}
+        //  horizontal
+        numColumns={3}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // justifyContent: "center",
+    // alignItems: "center",
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
 });
---
+++ src/TextInputModal.js
@@ -0,0 +1,33 @@
+import { Pressable, ViewComponent } from "react-native";
+import { KeyboardAvoidingView, SafeAreaView, TextInput } from "react-native";
+import { Modal, View } from "react-native";
+
+export default ({modalVisible , albumTitle, setAlbumTitle, onSubmitEditing, onPressBackdrop}) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+    >
+        <KeyboardAvoidingView  
+            behavior={Platform.OS === "ios" ? "padding" : "height"} 
+            style={{ flex: 1}}>
+            <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
+                <SafeAreaView style={{ flex: 1 , width: "100%", position: "absolute", bottom: 0}}>
+                     <TextInput
+                        placeholder="앨범명을 입력하세요" 
+                        style={{ width: "100%", backgroundColor: "lightblue",}}
+                        value={albumTitle}
+                        onChangeText={setAlbumTitle}
+                        onSubmitEditing={onSubmitEditing}
+                        autoFocus={true}
+                     >
+                        
+                     </TextInput>
+                </SafeAreaView>
+            </Pressable>
+           
+        </KeyboardAvoidingView >
+    </Modal>
+  );
+};
